d5d6e9ec9edc49274566f31b3d4e6c9f4f58a562
EstherAzevedo <estherazvdo123@gmail.com>
2023-07-31 15:02:19 -0300

    começando botão de repeat

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ const next = document.getElementById('next');
 const previous = document.getElementById('previous');
 const currentProgress = document.getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
+const shuffleButton = document.getElementById('shuffle');
+const repeatButton = document.getElementById('repeat');
 
+//array de musica
 const aThousandYears = {
   songName : 'a_thousand_years',
   artist : 'Christina Perri',
   file : 'a_thousand_years_christina_perri'
 };
 const boomBapFlick = {
-  songName : 'Perect',
+  songName : 'Perfect',
   artist : 'Ed Sheeran',
   file : 'ed_sheeran_perfect'
 };
@@ -23,8 +26,18 @@ const cantHide = {
   artist : 'Ed Sheeran',
   file : 'photograph_ed_sheeran'
 };
+const happyBirthday = {
+  songName : 'happy_birthday',
+  artist : 'THE KIBOOMERS',
+  file : 'happy_birthday'
+};
+
+
 let isPlaying = false;
-const playlist = [aThousandYears, boomBapFlick, cantHide];
+let isShuffled = false;
+let repeatOn = false;
+const originalPlaylist = [aThousandYears, boomBapFlick, cantHide, happyBirthday];
+let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
 function playSong(){
@@ -51,15 +64,15 @@ function playPauseDecider(){
 }
 
 function initializeSong(){
-  cover.src = `images/${playlist[index].file}.webp`;
-  song.src = `songs/${playlist[index].file}.mp3`;
-  songName.innerText = playlist[index].songName;
-  bandName.innerText = playlist[index].artist;
+  cover.src = `images/${sortedPlaylist[index].file}.webp`;
+  song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+  songName.innerText = sortedPlaylist[index].songName;
+  bandName.innerText = sortedPlaylist[index].artist;
 }
 
 function previousSong(){
   if(index === 0){
-    index = playlist.length - 1;
+    index = sortedPlaylist.length - 1;
   }else{
     index -= 1;
   }
@@ -68,7 +81,7 @@ function previousSong(){
 }
 
 function nextSong(){
-  if(index === playlist.length - 1){
+  if(index === sortedPlaylist.length - 1){
     index = 0;
   }else{
     index += 1;
@@ -89,6 +102,49 @@ function jumpTo(event){
   song.currentTime = jumpToTime;
 }
 
+function shuffleArray(preShuffleArray){
+  const size = preShuffleArray.length;
+  let currentIndex = size - 1;
+  while(currentIndex > 0){
+    let randonIndex = Math.floor(Math.random()* size);
+    let aux = preShuffleArray[currentIndex];
+    preShuffleArray[currentIndex] = preShuffleArray[randonIndex];
+    preShuffleArray[randonIndex] = aux;
+    currentIndex -= 1;
+  }
+}
+
+function shuffleButtonClicked(){
+  if(isShuffled === false){
+    isShuffled = true;
+    shuffleArray(sortedPlaylist);
+    shuffleButton.classList.add('button-active');
+  }
+  else{
+    isShuffled = false;
+    sortedPlaylist = [...originalPlaylist];
+    shuffleButton.classList.romove('button-active');
+  }
+}
+
+function repeatButtonClicked(){
+  if(repeatOn === false){
+    repeatOn = true;
+    repeatButton.classList.add('button-active');
+  }else{
+    repeatOn = false;
+    repeatButton.classList.remove('button-active');
+  }
+}
+
+function nextOrRepeat(){
+  if(repeatOn === false){
+    nextSong();
+  }else{
+    playSong();
+  }
+}
+
 initializeSong();
 
 //capacidade de escutar eventos
@@ -96,4 +152,7 @@ play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
 song.addEventListener('timeupdate', updateProgressBar);
+song.addEventListener('ended', nextOrRepeat)
 progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked);
+repeatButton.addEventListener('click', repeatButtonClicked);
