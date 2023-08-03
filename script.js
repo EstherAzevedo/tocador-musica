9cdd368f94b7dbf1601e47d977fdb03821367cc3
EstherAzevedo <estherazvdo123@gmail.com>
2023-08-03 14:36:29 -0300

    Finalizando

--- script.js
+++ script.js
@@ -5,40 +5,48 @@ const cover =  document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const likeButton = document.getElementById('like');
 const currentProgress = document.getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
 const shuffleButton = document.getElementById('shuffle');
 const repeatButton = document.getElementById('repeat');
+const songTime = document.getElementById('song-time');
+const totalTime = document.getElementById('total-time');
 
 //array de musica
 const aThousandYears = {
   songName : 'a_thousand_years',
   artist : 'Christina Perri',
-  file : 'a_thousand_years_christina_perri'
+  file : 'a_thousand_years_christina_perri',
+  liked : false,
 };
 const boomBapFlick = {
   songName : 'Perfect',
   artist : 'Ed Sheeran',
-  file : 'ed_sheeran_perfect'
+  file : 'ed_sheeran_perfect',
+  liked : true,
 };
 const cantHide = {
   songName : 'Photograph',
   artist : 'Ed Sheeran',
-  file : 'photograph_ed_sheeran'
+  file : 'photograph_ed_sheeran',
+  liked : true,
 };
 const happyBirthday = {
   songName : 'happy_birthday',
   artist : 'THE KIBOOMERS',
-  file : 'happy_birthday'
+  file : 'happy_birthday',
+  liked : true,
 };
 
 
 let isPlaying = false;
 let isShuffled = false;
 let repeatOn = false;
-const originalPlaylist = [aThousandYears, boomBapFlick, cantHide, happyBirthday];
+const originalPlaylist = JSON.parse(localStorage.getItem('playlist')) ?? [a_thousand_years_christina_perri, photograph_ed_sheeran, photograph_ed_sheeran, happy_birthday];
 let sortedPlaylist = [...originalPlaylist];
 let index = 0;
+
 
 function playSong(){
   play.querySelector('.bi').classList.remove('bi-play-circle-fill');
@@ -63,11 +71,25 @@ function playPauseDecider(){
   }
 }
 
+function likeButtonRender() {
+  if (sortedPlaylist[index].liked === true){
+      likeButton.querySelector(".bi").classList.remove("bi-heart");
+      likeButton.querySelector(".bi").classList.add("bi-heart-fill");
+      likeButton.classList.add("button-active");
+  }
+  else {
+      likeButton.querySelector(".bi").classList.add("bi-heart");
+      likeButton.querySelector(".bi").classList.remove("bi-heart-fill");
+      likeButton.classList.remove("button-active");
+  }
+}
+
 function initializeSong(){
   cover.src = `images/${sortedPlaylist[index].file}.webp`;
   song.src = `songs/${sortedPlaylist[index].file}.mp3`;
   songName.innerText = sortedPlaylist[index].songName;
   bandName.innerText = sortedPlaylist[index].artist;
+  likeButtonRender();
 }
 
 function previousSong(){
@@ -90,9 +112,10 @@ function nextSong(){
   playSong();
 }
 
-function updateProgressBar(){
+function updateProgress(){
   const barWidth = (song.currentTime/song.duration)*100;
   currentProgress.style.setProperty('--progress', `${barWidth}%`)
+  songTime.innerText = toHHMMSS(song.currentTime);
 }
 
 function jumpTo(event){
@@ -123,7 +146,7 @@ function shuffleButtonClicked(){
   else{
     isShuffled = false;
     sortedPlaylist = [...originalPlaylist];
-    shuffleButton.classList.romove('button-active');
+    shuffleButton.classList.remove("button-active");
   }
 }
 
@@ -145,14 +168,42 @@ function nextOrRepeat(){
   }
 }
 
+function toHHMMSS(originalNumber){
+  let hours = Math.floor(originalNumber/3600);
+  let min = Math.floor((originalNumber - hours * 3600)/60);
+  let secs = Math.floor(originalNumber - hours* 3600 - min* 60);
+
+  return `${hours.toString().padStart(2, '0')}:${min
+    .toString()
+    .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+}
+
+function updateTotalTime(){
+  totalTime.innerText = toHHMMSS(song.duration);
+}
+
+function likeButtonClicked() {
+  if (sortedPlaylist[index].liked === false){
+      sortedPlaylist[index].liked = true;
+  }
+  else {
+      sortedPlaylist[index].liked = false;
+  }
+  likeButtonRender();
+  localStorage.setItem("playlist", JSON.stringify(originalPlaylist));
+}
+
+
 initializeSong();
 
 //capacidade de escutar eventos
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
-song.addEventListener('timeupdate', updateProgressBar);
-song.addEventListener('ended', nextOrRepeat)
+song.addEventListener('timeupdate', updateProgress);
+song.addEventListener('ended', nextOrRepeat);
+song.addEventListener('loadedmetadata', updateTotalTime);
 progressContainer.addEventListener('click', jumpTo);
 shuffleButton.addEventListener('click', shuffleButtonClicked);
 repeatButton.addEventListener('click', repeatButtonClicked);
+likeButton.addEventListener('click', likeButtonClicked)
